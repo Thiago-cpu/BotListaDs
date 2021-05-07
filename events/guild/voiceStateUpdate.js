695dc180d2b76e523c2d572776a205e27681d273
Tomás Canosa Moreno <tomimoreno03@gmail.com>
2021-05-07 23:15:29 +0000

    Arreglando voiceStateUpdate parte 4

--- events/guild/voiceStateUpdate.js
+++ events/guild/voiceStateUpdate.js
@@ -1,17 +1,23 @@
 
 module.exports = (Discord, client, oldState, newState) => {
-  if (newState.channelID != oldState.channelID && client.listas.length){
-    const listasActuales = Object.keys(client.listas)
+  const listasActuales = Object.keys(client.listas)
+  if (newState.channelID != oldState.channelID && listasActuales.length){
     listasActuales.forEach((profesorId) =>{
       if(oldState.channelID === client.listas[profesorId].canalID ||
       newState.channelID === client.listas[profesorId].canalID){
         const lista = client.listas[profesorId] 
+        let nombre = newState.member.displayName
+        if(lista && lista.miembros[nombre]){
+          const horaActual = new Date().toISOString().substr(11, 8)
+          if (lista.miembros[nombre].length % 2 === 1){
+            console.log(`${nombre} salió de ${lista.curso} a las ${horaActual}`)
+          } else {
+            console.log(`${nombre} entró a ${lista.curso} a las ${horaActual}`)
+          }
+          lista.miembros[nombre].push(Date.now())
+        }
       }
     })
-    let nombre = newState.member.displayName
-    if(lista && lista.miembros[nombre]){
-      lista.miembros[nombre].push(Date.now())
-    }
   }
 }
 
